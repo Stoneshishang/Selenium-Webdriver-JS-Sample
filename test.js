2c47346d3e7adfa7267d782a8354c59452c4fe25
Shang Shi <shangs@XinningNinas-MacBook-Pro.local>
2021-01-08 16:47:19 -0500

    change key to Key to fix the bug

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
+require('chromedriver');
 const {Builder, By, Key, util} = require("selenium-webdriver");
-require("chromedriver");
 
 async function example(){
   let driver = await new Builder().forBrowser("chrome").build();
   await driver.get("http://google.com");
-  await driver.findElement(By.name("q")).sendKeys("Selenium Test", key.RETURN);
+  await driver.findElement(By.name("q")).sendKeys("Selenium Test", Key.RETURN);
 }
 
 example();
